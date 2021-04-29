057c71d1289c637440e182406000eda2c75d8152
WendelSantosD <wendelwcsantos@gmail.com>
2021-04-29 03:43:36 -0300

    3D

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,41 +1,130 @@
 package engineTester;
 
+import models.RawModel;
+import models.TexturedModel;
+
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
 
 public class MainGameLoop {
+
 	public static void main(String[] args) {
+
 		DisplayManager.createDisplay();
-		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {
-				// Left bottom triangle
-			    -0.5f, 0.5f, 0f,
-			    -0.5f, -0.5f, 0f,
-			    0.5f, -0.5f, 0f,
-			    // Right top triangle
-			    0.5f, -0.5f, 0f,
-			    0.5f, 0.5f, 0f,
-			    -0.5f, 0.5f, 0f
-			  };
+		float[] vertices = {			
+				-0.5f,0.5f,0,	
+				-0.5f,-0.5f,0,	
+				0.5f,-0.5f,0,	
+				0.5f,0.5f,0,		
+				
+				-0.5f,0.5f,1,	
+				-0.5f,-0.5f,1,	
+				0.5f,-0.5f,1,	
+				0.5f,0.5f,1,
+				
+				0.5f,0.5f,0,	
+				0.5f,-0.5f,0,	
+				0.5f,-0.5f,1,	
+				0.5f,0.5f,1,
+				
+				-0.5f,0.5f,0,	
+				-0.5f,-0.5f,0,	
+				-0.5f,-0.5f,1,	
+				-0.5f,0.5f,1,
+				
+				-0.5f,0.5f,1,
+				-0.5f,0.5f,0,
+				0.5f,0.5f,0,
+				0.5f,0.5f,1,
+				
+				-0.5f,-0.5f,1,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1
+				
+		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		float[] textureCoords = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
 		
-		while(!Display.isCloseRequested()) {
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+		
+		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
+		
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("image")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		
+		Camera camera = new Camera();
+		
+		while(!Display.isCloseRequested()){
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
-			//game logic
-			renderer.render(model);
+			shader.start();
+			shader.loadViewMatrix(camera);
+			renderer.render(entity,shader);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
-		
+
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
+
 	}
 
 }
